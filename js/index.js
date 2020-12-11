3550090b7a93b220f53c724189cd1385e109e428
pulk66-s <hugo.marrasse@epitech.eu>
2020-12-11 12:49:31 +0100

    try node

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 function get_token()
 {
     let token = document.getElementById("token_input").value;
+    let express = require("express")
+    let mysql = require("mysql")
+    let connect = mysql.createConnection({
+        host    : "localhost",
+        user    : "root",
+        password: "",
+        database: "sampleDB"
+    });
+    let app = express();
+
+    app.listen(1337)
+    connect.connect((err) => {
+        if (err)
+            console.log("Error");
+        else
+            console.log("Connected")
+    });
+    
+
     var tag = document.createElement("p");
     var text = document.createTextNode("Invalid token");
     tag.appendChild(text);
